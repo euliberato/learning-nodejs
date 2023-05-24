3da26dfb21eb854bb96ef7ba9312768eb9ba8a50
Ruan Liberato <88631449+euliberato@users.noreply.github.com>
2023-05-24 15:45:40 -0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app/app.js"
-
-const PORT = 3000
-
-//Express escuta a porta 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
-})   
